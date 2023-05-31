44a05f9892083c28943cd4281dfaf9552ec594ee
Hilal Ahmad <hilaldev123@gmail.com>
2023-05-31 12:49:34 +0500

    Search Bug

--- controllers/search.php
+++ controllers/search.php
@@ -6,8 +6,9 @@ $db = new Database();
 
 if(isset($_POST['search-button']))
 {
-    $posts = $db->query("SELECT *, DATE_FORMAT(time, '%e %M %Y %l:%i %p') AS formatted_time FROM posts
-    WHERE laptop_name REGEXP '[[:<:]]{$_POST['search']}[[:>:]]';")->fetchAll();
+    $posts = $db->query("SELECT *, DATE_FORMAT(time, '%e %M %Y %l:%i %p') AS formatted_time
+    FROM posts
+    WHERE laptop_name LIKE '%{$_POST['search']}%'")->fetchAll();
 }
 
 require 'views/search.view.php';
