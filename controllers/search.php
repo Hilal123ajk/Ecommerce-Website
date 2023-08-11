22b56b1c290bd5415a3768cb7fe6de082e0aab3c
Hilal Ahmad <hilaldev123@gmail.com>
2023-08-11 10:56:07 +0500

    Search Security Imporoved + Footer Added

--- controllers/search.php
+++ controllers/search.php
@@ -8,7 +8,9 @@ if(isset($_POST['search-button']))
 {
     $posts = $db->query("SELECT *, DATE_FORMAT(time, '%e %M %Y %l:%i %p') AS formatted_time
     FROM posts
-    WHERE laptop_name LIKE '%{$_POST['search']}%'")->fetchAll();
+    WHERE laptop_name LIKE :search", [
+        'search' => '%' . $_POST['search'] . '%'
+    ])->fetchAll();
 }
 
 require 'views/search.view.php';
--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,6 +1,36 @@
 </div>
 
 
+
+<footer class="bg-gray-900 rounded-lg shadow text-white m-4">
+    <div class="w-full mx-auto p-4 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <a href="/" class="flex items-center mb-4 sm:mb-0">
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Online Selling & Buying</span>
+            </a>
+            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium  sm:mb-0 text-white">
+                <li>
+                    <a href="/" class="mr-4 hover:underline md:mr-6 ">Home</a>
+                </li>
+                <li>
+                    <a href="user-login" class="mr-4 hover:underline md:mr-6 ">Login</a>
+                </li>
+                <li>
+                    <a href="/user-register" class="mr-4 hover:underline md:mr-6">Register</a>
+                </li>
+                <li>
+                    <a href="/user-guide" class="mr-4 hover:underline md:mr-6 ">Website Flow</a>
+                </li>
+            </ul>
+        </div>
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span class="block text-sm  sm:text-center text-white">© 2023 <a href="" class="hover:underline">Copywrite</a>. All Rights Reserved.</span>
+    </div>
+</footer>
+
+
+
+
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </body>
 </html>
